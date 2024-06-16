e22502a650f1d382b65dc75aff1479eec2609411
CGARCHER <cipri.profesor.informatica@gmail.com>
2024-06-16 17:15:35 +0200

    otro

--- appCipri/src/main/java/com/example/demo/AppCipriApplication.java
+++ appCipri/src/main/java/com/example/demo/AppCipriApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AppCipriApplication {
 
-	//hola
+	//holad
 	public static void main(String[] args) {
 		SpringApplication.run(AppCipriApplication.class, args);
 	}
